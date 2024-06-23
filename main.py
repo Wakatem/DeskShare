e58e0f689b0bb22e99ecceaca14dd9d91ce22392
Wakatem <moody-8@live.com>
2024-06-24 02:51:43 +0400

    Add FPS options menu and dynamically update selected FPS

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import time
 import cv2
 import numpy as np
 from screeninfo import get_monitors
-from pyvda import AppView, get_apps_by_z_order, VirtualDesktop, get_virtual_desktops
+from pyvda import VirtualDesktop, get_virtual_desktops
 from pystray import Icon as icon, Menu as menu, MenuItem as item
 from PIL import Image
 import ctypes
 import threading
 from multiprocessing import Process, Value
-from pygame.locals import *
-
+import configparser
 
 def resize_frame(frame, target_width=1920):
     # Convert raw bytes to a numpy array
@@ -36,21 +35,38 @@ def compress_frame(frame, quality):
     else:
         img_np = frame
     
-    # Resize the frame
-    # height, width = img_np.shape[:2]
-    # target_height = int((target_width / width) * height)
-    # resized_img = cv2.resize(img_np, (target_width, target_height))
-    
     # Compress the frame (e.g., JPEG compression)
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
-    _, compressed_img = cv2.imencode('.jpg', frame, encode_param)
+    _, compressed_img = cv2.imencode('.jpg', img_np, encode_param)
     
     return cv2.imdecode(compressed_img, 1)
 
+def load_config_ini(path):
+    config = configparser.ConfigParser()
+    try:
+        config.read(path)
+        config['settings']
+    except Exception as e:
+        print(f"An error occurred while reading the configuration file: {e}")
+        print("Creating a new configuration file")
+
+        config.add_section('settings')
+        config.set('settings', 'fps', '10')
+        config.write(open(path, 'w'))
+
+    return config
 
 
-def get_window_coordinates(window_title):
-    # Get the window object for the specific app
+def update_config_ini(path, section, key, value):
+    config = configparser.ConfigParser()
+    try:
+        config.read(path)
+        config[section][key] = value
+        config.write(open(path, 'w'))
+    except Exception as e:
+        print(f"An error occurred while updating the configuration file: {e}")
+
+def get_window_object(window_title):
     window = gw.getWindowsWithTitle(window_title)
     if window:
         win = window[0]
@@ -62,9 +78,7 @@ def get_window_coordinates(window_title):
 
 
 def share_desktop(fps, stop_sharing, selected_desktop):
-    # global fps, stop_sharing, selected_desktop
 
-    frame_interval = 1.0 / fps
     primary_monitor = get_monitors()[0]
     left, top, width, height = primary_monitor.x, primary_monitor.y, primary_monitor.width, primary_monitor.height
     
@@ -76,6 +90,7 @@ def share_desktop(fps, stop_sharing, selected_desktop):
     with mss.mss() as sct:
 
         while not stop_sharing.value:
+            frame_interval = 1.0 / fps.value
             activeWindow = gw.getActiveWindow()
 
             # Avoid capturing the Task View window
@@ -86,9 +101,21 @@ def share_desktop(fps, stop_sharing, selected_desktop):
                     imgUMAT = cv2.UMat(img)
 
                     cv2.imshow("DeskShare", imgUMAT)
+            
+            # Print calculated fps
+            # calculated_fps = 1.0 / (current_time - last_frame_time)
+            
+            # # Move cursor up one line and clear line
+            # print("\033[A\033[K", end="")  # Move up and clear the line
+            # print(f"FPS: {calculated_fps:.2f}", end="\r\n")  # Print new first line
+
+            # # Clear the next line and print
+            # print("\033[K", end="")  # Clear the line
+            # print(f"Active Window: {activeWindow.title if activeWindow else 'None'}", end="\r")
 
             # Calculate sleep time to maintain target FPS
-            sleep_time = frame_interval - (time.time() - last_frame_time)
+            current_time = time.time()
+            sleep_time = frame_interval - (current_time - last_frame_time)
             if sleep_time > 0:
                 time.sleep(sleep_time)
 
@@ -132,6 +159,21 @@ def select_desktop(number):
     return inner
 
 
+def select_fps(fps):
+    def inner(icon, item):
+        global fps
+        fps.value = int(item.text)
+        icon.update_menu()
+        update_config_ini('config.ini', 'settings', 'fps', item.text)
+    return inner
+
+def get_selected_fps(fps):
+    def inner(item):
+        global fps
+        return fps.value == int(item.text)
+    return inner
+
+
 def on_stop_sharing(icon, item):
     global stop_sharing, selected_desktop
     stop_sharing.value = True
@@ -149,9 +191,10 @@ def quit(icon, item):
 if __name__ == '__main__':
     # Make the application DPI aware to handle display scaling properly
     ctypes.windll.shcore.SetProcessDpiAwareness(2)
+    config = load_config_ini('config.ini')
 
     selected_desktop = 0
-    fps = 24
+    fps = Value('i', int(config['settings']['fps']))
     stop_sharing = Value('b', False)
     quit_program = False
 
@@ -159,9 +202,14 @@ if __name__ == '__main__':
     desktop_items = [item(f"Desktop {desktop.number}", select_desktop(desktop.number), checked=get_selected_desktop(desktop.number), radio=True) for desktop in get_virtual_desktops()]
     desktops_menu = menu(*desktop_items)
 
+    fps_items = [item(str(i+5), action=select_fps(i+5), checked=get_selected_fps(i+5), radio=True) for i in range(0, 56, 5)]
+    fps_menu = menu(*fps_items)
+
     items_menu = menu(
-        item('Start Sharing', desktops_menu),
+        item('Start Sharing', desktops_menu, enabled=lambda item: selected_desktop == 0),
         item('Stop Sharing', on_stop_sharing, enabled=lambda item: selected_desktop != 0),
+        menu.SEPARATOR,
+        item('FPS', fps_menu),
         item('Quit', quit)
     )
 
